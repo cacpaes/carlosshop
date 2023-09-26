0ae363bdd2a2d0cafb57dd6aa13ea98f4ce0ea79
CACPaes <93129370+CACPaes@users.noreply.github.com>
2023-09-26 12:20:14 +0100

    Create repository for employee

--- CarlosShop/Program.cs
+++ CarlosShop/Program.cs
@@ -7,16 +7,48 @@ var customer = new Customers();
 
 Console.WriteLine("Informe o Id");
 customer.Id = Convert.ToInt32(Console.ReadLine());
-
-customer.Name = "Carlos Carvalho";
-customer.Email = "cacarvalho.paes@gmail.com";
+Console.WriteLine("Informe o Name");
+customer.Name = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe o Email");
+customer.Email = Convert.ToString(Console.ReadLine());
 Console.WriteLine("Informe nova data");
 customer.DateBirth = Convert.ToDateTime(Console.ReadLine());
-customer.Address = "41 Temple Place";
-customer.HasCard = true;
-customer.CreditLimit = 100;
-customer.PhoneNumber = "0834187086";
-customer.PublicServiceId = "13809837709";
+Console.WriteLine("Informe o Adress");
+customer.Address = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe se tem Cartão");
+customer.HasCard = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("Informe o Credit Limit");
+customer.CreditLimit = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Informe o Phone Number");
+customer.PhoneNumber = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe o Public Service Id");
+customer.PublicServiceId = Convert.ToString(Console.ReadLine());
 
 repositoryCustomer.Create(customer);
+
+
+var repositoryEmployee = new RepositoryEmployee();
+
+var employee = new Employees();
+
+Console.WriteLine("Informe o Id");
+employee.Id = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Informe o Name");
+employee.Name = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe o Email");
+employee.Email = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe a Data do Aniversario");
+employee.DateBirth = Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Informe o Adress");
+employee.Address = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe Quanto e o Salario");
+employee.Salary = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Informe a Data data de Inicio");
+employee.StartDate = Convert.ToDateTime(Console.ReadLine());
+Console.WriteLine("Informe o Phone Number");
+employee.PhoneNumber = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Informe o Public Service Id");
+employee.PublicServiceId = Convert.ToString(Console.ReadLine());
+
+repositoryEmployee.Create(employee);
 
